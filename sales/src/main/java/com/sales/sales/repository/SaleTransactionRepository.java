af6e7ff872a11c21979cd42ab0374f5a161ace5d
Reem <reemgharib97@outlook.com>
2024-02-23 08:25:52 -0800

    amend entities

--- sales/src/main/java/com/sales/sales/repository/SaleTransactionRepository.java
+++ sales/src/main/java/com/sales/sales/repository/SaleTransactionRepository.java
@@ -1,5 +1,9 @@
 package com.sales.sales.repository;
 
+import com.sales.sales.model.SaleTransaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 @Repository
 public interface SaleTransactionRepository extends JpaRepository<SaleTransaction, Long> {
 }
